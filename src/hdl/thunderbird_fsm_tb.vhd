31bf76c518da835a83bde8758d09bdf3149dc5d2
Jonathonw478 <C26Jonathon.Watson@user.noreply.github.com>
2024-03-22 09:55:20 -0600

    Lab3

--- src/hdl/thunderbird_fsm_tb.vhd
+++ src/hdl/thunderbird_fsm_tb.vhd
@@ -56,29 +56,113 @@ end thunderbird_fsm_tb;
 
 architecture test_bench of thunderbird_fsm_tb is 
 	
+	-- component declaration for the unit under test (UUT)
+	
 	component thunderbird_fsm is 
 	  port(
-		
+          i_clk, i_reset  : in    std_logic;
+          i_left, i_right : in    std_logic;
+          o_lights_L      : out   std_logic_vector(2 downto 0);
+          o_lights_R      : out   std_logic_vector(2 downto 0)
 	  );
-	end component thunderbird_fsm;
+    end component thunderbird_fsm;        
 
 	-- test I/O signals
 	
+	-- inputs
+	signal w_clk   : std_logic := '0';
+	signal w_reset : std_logic := '0';
+	signal w_left  : std_logic := '0';
+	signal w_right : std_logic := '0';
+	
+	-- outputs
+	signal w_lights_L  : std_logic_vector(2 downto 0)  := "000";
+	signal w_lights_R  : std_logic_vector(2 downto 0)  := "000";
+	
+	-- signal w_thunderbird   :   std_logic_vector(2 downto 0) := "000";
+	
 	-- constants
+	constant k_clk_period  : time := 10ns;
 	
 	
 begin
 	-- PORT MAPS ----------------------------------------
-	
+	--Instantiate the unit under test (UUT)
+	uut: thunderbird_fsm port map (
+        i_clk => w_clk,
+        i_reset => w_reset,
+        i_left => w_left,
+        i_right => w_right,
+        o_lights_L => w_lights_L,
+        o_lights_R => w_lights_R
+	);
 	-----------------------------------------------------
 	
 	-- PROCESSES ----------------------------------------	
     -- Clock process ------------------------------------
-    
+    clk_proc : process
+    begin
+        w_clk <= '0';
+        wait for k_clk_period/2;
+        w_clk <= '1';
+        wait for k_clk_period/2;
+    end process;
 	-----------------------------------------------------
 	
 	-- Test Plan Process --------------------------------
+	tp_proc: process
+	begin
 	
+        -- sequential timing
+        w_reset <= '1';
+        wait for k_clk_period*1;
+           assert w_lights_L = "000" report "bad reset" severity failure;
+           assert w_lights_R = "000" report "bad reset" severity failure;
+        
+        w_reset <= '0';
+        wait for k_clk_period*1;
+        
+        -- left turn
+        w_left <= '1'; w_right <= '0';
+        wait for k_clk_period;
+           assert w_lights_L = "001" report "1st stage of left turn signal failed" severity failure;
+           assert w_lights_R = "000" report "1st stage of right output from left turn signal failed" severity failure;
+        wait for k_clk_period;
+           assert w_lights_L = "011" report "2nd stage of left turn signal failed" severity failure;
+           assert w_lights_R = "000" report "2nd stage of right output from left turn signal failed" severity failure;
+        wait for k_clk_period;
+           assert w_lights_L = "111" report "3rd stage of left turn signal failed" severity failure;
+           assert w_lights_R = "000" report "3rd stage of right output from left turn signal failed" severity failure;
+        wait for k_clk_period;
+           assert w_lights_L = "000" report "final stage / reset of left turn signal failed" severity failure;
+           assert w_lights_R = "000" report "final stage / reset of left turn signal failed" severity failure;            
+       
+        -- right turn
+        w_left <= '0'; w_right <= '1';
+        wait for k_clk_period;
+           assert w_lights_L = "000" report "1st stage of left output from right turn signal failed" severity failure;
+           assert w_lights_R = "001" report "1st stage of right turn signal failed" severity failure;
+        wait for k_clk_period;
+           assert w_lights_L = "000" report "2nd stage of left output from right turn signal failed" severity failure;
+           assert w_lights_R = "011" report "2nd stage of right turn signal failed" severity failure;
+        wait for k_clk_period;
+           assert w_lights_L = "000" report "3rd stage of left output from right turn signal failed" severity failure;
+           assert w_lights_R = "111" report "3rd stage of right turn signal failed" severity failure;
+        wait for k_clk_period;
+           assert w_lights_L = "000" report "final stage / reset of left turn signal failed" severity failure;
+           assert w_lights_R = "000" report "final stage / reset of left turn signal failed" severity failure;     
+       
+       -- hazards 
+       w_left <= '1'; w_right <= '1';
+       wait for k_clk_period;
+           assert w_lights_L = "111" report "left lights failed to light up during hazards stage" severity failure;
+           assert w_lights_R = "111" report "right lights failed to light up during hazards stage" severity failure;
+       wait for k_clk_period;
+           assert w_lights_L = "000" report "left lights failed to turn off during hazards stage" severity failure;
+           assert w_lights_R = "000" report "right lights failed to turn off during hazards stage" severity failure;
+           
+           wait;
+       end process;
 	-----------------------------------------------------	
 	
 end test_bench;
